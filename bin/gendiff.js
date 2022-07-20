e14005dcf9c73b065960c132884d79bd262fc2c2
Dmitry Kuznecov <dimakuznet030@gmail.com>
2022-07-20 11:20:08 +0300

    fix gendiif

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -12,6 +12,6 @@ program
     .arguments('<filepath1> <filepath2>')
     .action((filepath1, filepath2, { format }) => {
         console.log(genDiff(filepath1, filepath2, format));
-    });
+      });
 
 program.parse();
