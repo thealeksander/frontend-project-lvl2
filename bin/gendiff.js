fcb5aa7df0bcd16687a2657a2310fa96811f4f58
Dmitry Kuznecov <dimakuznet030@gmail.com>
2022-07-22 11:35:51 +0300

    install eslint

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,10 +5,10 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
-    .name('gendiff')
-    .description('Compares two configuration files and shows a difference.')
-    .version('0.1', '-V, --version', 'output the version number')
-    .option('-f, --format <type>', 'output format')
-    .argument('<filepath1> <filepath2>')
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.1', '-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1> <filepath2>');
 
 program.parse();
