fe69c551e02a01b6f970dce1ce7bfb0984e09592
Dmitry Kuznecov <dimakuznet030@gmail.com>
2022-08-20 13:49:17 +0300

    finish test

--- Makefile
+++ Makefile
@@ -2,3 +2,5 @@ lint: #запускает eslint
 	npx eslint
 install: #установка npm ci
 	npm ci
+test: #запуск тестов
+	npx jest
--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,19 +3,32 @@ import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import * as fs from 'node:fs';
 import { expect } from '@jest/globals';
-import { readFile } from 'fs';
+
+let readFile;
+let __dirname;
+let __filename;
+let getFixturePath;
 
 beforeAll(() => {
-    const __filename = fileURLToPath(import.meta.utl);
-    const __dirname = dirname(__filename);
-    const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-    const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+    __filename = fileURLToPath(import.meta.url);
+    __dirname = dirname(__filename);
+    getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+    readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 });
 
-test('gendiff\'s main flow ', () => {
-    const expected = readFile('result.txt');
-    const filepath1 = getFixturePath('file1.json');
-    const filepath2 = getFixturePath('file2.json');
-    const actual = gendiff(filepath1, filepath2);
-    expect(actual).toBe(expected);
+describe('gendiff\'s main flow ', () => {
+    test('with json format', () => {
+        const expected = readFile('result.txt');
+        const filepath1 = getFixturePath('file1.json');
+        const filepath2 = getFixturePath('file2.json');
+        const actual = gendiff(filepath1, filepath2);
+        expect(actual).toBe(expected);
+    });
+    test('with yaml format', () => {
+        const expected = readFile('result.txt');
+        const filepath1 = getFixturePath('file1.yaml');
+        const filepath2 = getFixturePath('file2.yaml');
+        const actual = gendiff(filepath1, filepath2);
+        expect(actual).toBe(expected);
+    });
 });
