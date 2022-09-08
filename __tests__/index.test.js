110424abf935e51375f1a622ac3e9ac331bef6d8
Alexander <the7alexander7@gmail.com>
2022-09-08 15:12:07 +0300

    test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -21,14 +21,14 @@ describe('gendiff\'s main flow ', () => {
         const expected = readFile('result_stylish.txt');
         const filepath1 = getFixturePath('file1.json');
         const filepath2 = getFixturePath('file2.json');
-        const actual = gendiff(filepath1, filepath2, 'stylish');
+        const actual = gendiff(filepath1, filepath2);
         expect(actual).toBe(expected);
     });
     test('with yaml format', () => {
         const expected = readFile('result_stylish.txt');
         const filepath1 = getFixturePath('file1.yml');
         const filepath2 = getFixturePath('file2.yml');
-        const actual = gendiff(filepath1, filepath2, 'stylish');
+        const actual = gendiff(filepath1, filepath2);
         expect(actual).toBe(expected);
     });
 });
