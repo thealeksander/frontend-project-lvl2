00c5d5ba75082c1014d1e4db1e0aa5d6db6d2c95
Alexander <the7alexander7@gmail.com>
2022-09-14 21:18:50 +0300

    upgrade project

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import getdiff from './formatters/index.js';
 const getData = (filepath) => readFileSync(path.resolve(`${process.cwd()}`, filepath));
 const getExtension = (filepath) => path.extname(filepath).slice(1);
 
-const gendiff = (file1, file2, format = 'stylish') => {
-  const data1 = parse(getData(filepath1), getExtension(filepath1));
-  const data2 = parse(getData(filepath2), getExtension(filepath2));
+const gendiff = (filePath1, filePath2, format = 'stylish') => {
+  const data1 = parse(getData(filePath1), getExtension(filepath1));
+  const data2 = parse(getData(filePath2), getExtension(filepath2));
   return getdiff(buildTree(data1, data2), format);
 };
 
