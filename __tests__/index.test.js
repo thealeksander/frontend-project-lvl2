872fd199be01f74c581d70610022093157920dc7
Alexander <the7alexander7@gmail.com>
2022-09-19 17:46:06 +0300

    fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -18,16 +18,20 @@ const variants = [
   ['file1.yml', 'file2.yml', 'json'],
 ];
 
+const result_stylish = readFile('result_stylish.txt');
+const result_plain = readFile('result_plain.txt');
+const result_json = readFile('result_json.txt');
+
 test.each(variants)('gendiff-tests', (file1, file2, format = 'stylish') => {
   const actual = gendiff(getFixturePath(file1), getFixturePath(file2), format);
   const expected = (formatter) => {
     switch (formatter) {
       case 'stylish':
-        return readFile('result_stylish.txt');
+        return result_stylish;
       case 'plain':
-        return readFile('result_plain.txt');
+        return result_plain;
       case 'json':
-        return readFile('result_json.txt');
+        return result_json;
       default:
         throw new Error(`Unknown type of format: ${formatter}`);
     }
