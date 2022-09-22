3d75a459b7f784c864b2732744f2689ec4866fb3
Alexander <the7alexander7@gmail.com>
2022-09-22 17:45:30 +0300

    fixed

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,19 +10,19 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 const variants = [
-  ['file1.json', 'file2.json', 'stylish'],
-  ['file1.yml', 'file2.yml'],
-  ['file1.json', 'file2.json', 'plain'],
-  ['file1.yml', 'file2.yml', 'plain'],
-  ['file1.json', 'file2.json', 'json'],
-  ['file1.yml', 'file2.yml', 'json'],
+  { file1: 'file1.json',file2: 'file2.json', type: 'stylish'},
+  { file1: 'file1.yml', file2: 'file2.yml'},
+  { file1: 'file1.json',file2: 'file2.json', type: 'plain'},
+  { file1: 'file1.yml', file2: 'file2.yml', type: 'plain'},
+  { file1: 'file1.json',file2: 'file2.json', type: 'json'},
+  { file1: 'file1.yml', file2: 'file2.yml', type: 'json'},
 ];
 
 const resultStylish = readFile('result_stylish.txt');
 const resultPlain = readFile('result_plain.txt');
 const resultJson = readFile('result_json.txt');
 
-test.each(variants)('gendiff-tests', (file1, file2, type) => {
+test.each(variants)('gendiff-tests(%#)', ({file1, file2, type}) => {
   const actual = gendiff(getFixturePath(file1), getFixturePath(file2), type);
   const expected = (formatter) => {
     switch (formatter) {
